80e14e5af76342bccfbaf474349af41836ffae68
scotthowson <scott_howson@corruptiongames.net>
2024-07-30 14:01:11 -0400

    update Qcacld

--- drivers/staging/wlan-qc/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/wlan-qc/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -25,16 +25,16 @@
 
    BRIEF DESCRIPTION:
    WLAN Host Version file.
-   Build number automatically updated by build scripts.
+    Build number automatically updated by build scripts.
 
    ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
-#define QWLAN_VERSION_PATCH            022
-#define QWLAN_VERSION_EXTRA            "R"
-#define QWLAN_VERSION_BUILD            7
+#define QWLAN_VERSION_PATCH            03
+#define QWLAN_VERSION_EXTRA            "T"
+#define QWLAN_VERSION_BUILD            30
 
-#define QWLAN_VERSIONSTR               "5.2.022.7R"
+#define QWLAN_VERSIONSTR               "5.2.03.30T"
 
 #endif /* QWLAN_VERSION_H */
