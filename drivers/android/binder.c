75cb331980ada41f4bb0ccf882b46450d56cd165
Scott Howson <scott_howson@corruptiongames.net>
2025-05-05 01:48:19 -0400

    Configurable global PID lookups
    - and implements a kernel parameter to explicitly allow disabling
    the behavior change to look up caller PIDs in the global namespace.
    
    fix typo
    
    thanks:  Rúben Carneiro

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -4494,7 +4494,7 @@ retry:
 			trd->sender_pid =
 				task_tgid_nr_ns(sender,
 						task_active_pid_ns(current));
-				if (binder_global_pid_lookups && trd->sender_pid == 0)
+				(binder_global_pid_lookups && trd->sender_pid == 0)
 					trd->sender_pid = task_tgid_nr(sender);
 		} else {
 			trd->sender_pid = 0;
